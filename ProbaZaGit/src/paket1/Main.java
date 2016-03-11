c86ca2012fac0a663cba39b2ec065e9466cf749f
aleksandarcolic22414 <acacolic@gmail.com>
2016-03-11 19:42:26 +0100

    Dodali smo jes nesto.

--- ProbaZaGit/src/paket1/Main.java
+++ ProbaZaGit/src/paket1/Main.java
@@ -3,8 +3,8 @@ package paket1;
 public class Main {
 
 	public static void main(String[] args) {
-
-		System.out.println("Aleksandar Colic ");
+		System.out.println("Aleksandar Colic");
+		System.out.println("Milos Petrovic");
 	}
 
 }
