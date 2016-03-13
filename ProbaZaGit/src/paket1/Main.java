2cd0497220bc59cb2a464fb1942b01817da553b9
aleksandarcolic22414 <acacolic@gmail.com>
2016-03-13 15:00:11 +0100

    Opaaaaa

--- ProbaZaGit/src/paket1/Main.java
+++ ProbaZaGit/src/paket1/Main.java
@@ -5,6 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Aleksandar Colic");
 		System.out.println("Milos Petrovic");
+		System.out.println("Milos Petrovic");
+		System.out.println("Milos Petrovic");
+		System.out.println("Milos Petrovic");
+		System.out.println("Milos Petrovic");
+		System.out.println("Milos Petrovic");
+		System.out.println("Milos Petrovic");
+		System.out.println("Milos Petrovic");
 	}
 
 }
